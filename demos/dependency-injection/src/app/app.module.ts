f5940dfdc25834155b88e731e129bb30761f31e7
Michael Gustavsson <michael.gustavsson@skaftoskorpan.se>
2019-03-14 16:53:26 +0100

    Updated character service

--- demos/dependency-injection/src/app/app.module.ts
+++ demos/dependency-injection/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterListComponent } from './characters/character-list/character-list.component';
+import { CharacterService } from './characters/character.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { CharacterListComponent } from './characters/character-list/character-li
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
