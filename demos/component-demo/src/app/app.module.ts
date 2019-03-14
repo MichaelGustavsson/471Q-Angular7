b87138215a58d43e902ac8eabfb5179d0c07f2dd
Michael Gustavsson <michael.gustavsson@skaftoskorpan.se>
2019-03-14 14:32:48 +0100

    Initial Commit

---
+++ demos/component-demo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharacterListComponent } from './characters/character-list/character-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
